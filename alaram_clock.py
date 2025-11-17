c6cd25bb8085ff8eb665cd88d7a62723f316c7e8
nsivasu <nsivasu@gmail.com>
2025-11-17 10:22:35 +0530

    Updated

--- alaram_clock.py
+++ alaram_clock.py
@@ -1,21 +1,16 @@
-from datetime import datetime   
-from playsound import playsound
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
-alarm_seconds=alarm_time[6:8]
-alarm_period = alarm_time[9:11].upper()
-print("Setting up alarm..")
+import datetime
+import time
+import winsound
+
+alarm_time = input("Alarm time (HH:MM:SS): ")
+message = input("Your alarm message: ")
+
 while True:
-    now = datetime.now()
-    current_hour = now.strftime("%I")
-    current_minute = now.strftime("%M")
-    current_seconds = now.strftime("%S")
-    current_period = now.strftime("%p")
-    if(alarm_period==current_period):
-        if(alarm_hour==current_hour):
-            if(alarm_minute==current_minute):
-                if(alarm_seconds==current_seconds):
-                    print("Wake Up!")
-                    playsound('audio.mp3')
-                    break
+    if datetime.datetime.now().strftime("%H:%M:%S") == alarm_time:
+        print(" ALARM:", message)
+        
+        for _ in range(5):  # Repeat beep 5 times
+            winsound.Beep(1500, 700)
+            time.sleep(0.3)
+        break
+    time.sleep(1)
